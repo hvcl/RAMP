b27031cf8de57e90b59f592636fd6d876ee23ca4
leekanggeun <leekanggeun@gmail.com>
2022-11-28 16:48:53 +0900

    Update demo_tf.py

--- src/response_prediction/scripts/demo_tf.py
+++ src/response_prediction/scripts/demo_tf.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
     n_channels = 64
     Outer_fold = 10
     Inner_fold = 10
-    search_alpha_list = [10.0, 15.0, 20.0, 25.0, 30.0] # Average alpha instead of search list
+    search_alpha_list = [1.0, 2.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0] # Average alpha instead of search list
     search_dp_rate_list = [0.1, 0.2, 0.3, 0.4, 0.5] # Average dp rate instead of search list
     embedding_type = "rans"
     
