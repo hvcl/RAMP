e15ddb4fbeea33929894506b8301f3e7e808b98b
leekanggeun <leekanggeun@gmail.com>
2022-11-28 16:17:58 +0900

    Update RAMP_tf.py

--- src/response_prediction/models/RAMP_tf.py
+++ src/response_prediction/models/RAMP_tf.py
@@ -28,6 +28,7 @@ class Drug_Response_Predictor(keras.Model):
         self.h4_2 = layers.Dense(ndrugs, activation=None)
         self.alpha = alpha
         self.dp_rate = dp_rate
+        self.ce = tf.keras.losses.BinaryCrossentropy(from_logits=False)
         self.loss_tracker = keras.metrics.Mean(name="loss")
         self.loss_tracker.reset_states()
 
@@ -63,7 +64,8 @@ class Drug_Response_Predictor(keras.Model):
             logits = tf.linalg.matmul(features_normalized, tf.transpose(features_normalized))
             y_true = tf.linalg.matmul(label,label, transpose_b=True, a_is_sparse=True, b_is_sparse=True)
             y_true /= 265
-            contrastive_loss = self.alpha*tf.math.reduce_mean(-y_true*tf.math.log(logits))
+            #contrastive_loss = self.alpha*tf.math.reduce_mean(-y_true*tf.math.log(logits))
+            contrastive_loss = self.alpha*self.ce(y_ture, logits)
             ce_loss = tf.reduce_mean(mask*tf.nn.sigmoid_cross_entropy_with_logits(label, out))
             loss = ce_loss + contrastive_loss
         gradients = tape.gradient(loss, self.trainable_variables)
